95f2345cb49834247bdd818487f74f5ac4ce886d
Sunny Israni <sunnyisrani@Sunnys-MacBook-Pro.local>
2014-04-09 16:48:15 -0400

    setting up development workflow to compile and minify/uglify all css/js files and better organize assets

---
+++ Gruntfile.js
@@ -0,0 +1,86 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+  
+  watch: {
+    sass: {
+      files: 'styles/*.scss',
+      tasks: ['sass','cssmin']
+    },
+    scripts: {
+      files: 'scripts/*',
+      tasks: ['uglify']
+    },
+    images: {
+      files: 'images/*',
+      tasks: ['copy:images']
+    },
+    fonts: {
+      files: 'fonts/*',
+      tasks: ['copy:fonts']
+    }
+  },
+  clean: ['deploy/assets/*'],
+  sass: {                             
+      dist:{                       
+        options: {
+          style: 'expanded'
+        },
+      files: {
+        'styles/style.css' : 'styles/style.scss'
+      }
+    }
+  },
+  cssmin: {
+    minify: {
+      files: {
+        'deploy/assets/style.min.css' : 'styles/style.css' 
+      }
+    }
+  },
+  uglify: {
+     my_target: {
+      files: {
+        'deploy/assets/script.min.js': 'scripts/*'
+      }
+    }
+  },
+  copy: {
+    images: {
+      expand: true,
+      cwd: 'images/',
+      src: '**',
+      dest: 'deploy/assets/',
+      flatten: true,
+      filter: 'isFile'
+    },
+    fonts: {
+      expand: true,
+      cwd: 'fonts/',
+      src: '**',
+      dest: 'deploy/assets/',
+      flatten: true,
+      filter: 'isFile'
+    }
+  }
+});
+
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  
+  grunt.registerTask('build', [
+    'clean',
+    'sass',
+    'cssmin',
+    'uglify',
+    'copy'
+    ]);
+  
+  grunt.registerTask('default', ['sass']);
+
+};
