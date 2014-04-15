f8c14beb8b1fcd234932abcc2c657ea9a267ffe7
Sunny Israni <sunnyisrani@Sunnys-MacBook-Pro.local>
2014-04-15 11:27:15 -0400

    adding collection page: collection grid, filters, and quickview

---
+++ scripts/filter.js
@@ -0,0 +1,58 @@
+var filter = function() {
+  $('.select-filter').click(function(event){
+    
+    event.preventDefault();
+    tags = getTags($(this))
+    selectedTag = $(this).attr('id')
+    url = window.location.pathname
+    if (tagSelected(url,selectedTag) == true){
+      newUrl = removalUrl(url,selectedTag)
+    } 
+    else if (filteredSelection(url,tags) == true) {
+      regExpTags = new RegExp(tags.join('|'))
+      newUrl = url.replace(regExpTags,selectedTag);
+    }
+    else if (noFilters(url)) {
+      newUrl = url + '/' + selectedTag
+    } 
+    else {
+      newUrl = url + '+' + selectedTag
+    }
+    window.location.href = newUrl
+  })
+}
+
+var getTags = function(el) {
+  ids = []
+  el.parent().find('a').each(function() {ids.push(this.id)})
+  return ids
+}
+
+var filteredSelection = function(url,tags){
+  present = false
+  $.each(tags,function(key,tag){
+    if (url.search(tag) >= 0) {
+      present = true
+    }
+  })
+  return present
+}
+
+var noFilters = function(url) {
+  return $(url.split('/')).last()[0] == 'all'
+}
+
+var tagSelected = function(url,tag){
+  return url.search(tag) >= 0
+}
+
+var removalUrl = function(url,tag){
+  if ($(url.split('/')).last()[0] == tag){
+    newUrl = url.replace('/'+tag,'')
+  } else if (url.search('/'+tag) >= 0){
+    newUrl = url.replace(tag+'+','')
+  } else {
+    newUrl = url.replace('+'+tag,'')
+  }
+  return newUrl
+}
