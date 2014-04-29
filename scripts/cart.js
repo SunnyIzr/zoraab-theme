5947e143df4fad98bf7402e5a7a03ea62fa25ac4
Mohit Ramchandani <Mohitr35@gmail.com>
2014-04-29 11:58:07 -0400

    minor fixes; cart # displays right,checkout is greyed out when empty cart,right arrow is added, now working on ajaxing into cart.

--- scripts/cart.js
+++ scripts/cart.js
@@ -1,3 +1,21 @@
 $(function() {
-  
+  if ($('#cart')[0].innerText == " CART (0) ")
+  {
+  	$('#co').addClass("empty-cart");
+  	$('#co').removeClass("button-co");
+  }
+
+  $(".remove-button").click(function(e){
+  	e.preventDefault();
+  	var url = $(this).parent()[0].href
+  	$.get(url)
+  	$(this).parent().parent().remove()
+  	$.getJSON("/cart.js",function(response){ 
+  		var item_count = response.item_count
+  		$('#item-count')[0].innerText = item_count
+
+  	});
+
+  });
+
 });
