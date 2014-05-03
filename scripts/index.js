89f324eaff52abe37d512f194912e1eaf27ce5a3
Sunny Israni <sunny@zoraab.com>
2014-05-03 16:21:57 -0400

    fixing hover state on index grid

--- scripts/index.js
+++ scripts/index.js
@@ -5,10 +5,12 @@ $(document).ready(function(){
 
 var panelHoverEffect = function(){
   $('.linked-panel').hover(function(){
-    $(this).find('p').attr('color','#67c5a9');
+    $(this).find('.text').css('color','#67c5a9');
+    $(this).find('.link').css('color','#67c5a9');
     $(this).find('path').attr('fill','#67c5a9');
   }, function(){
-    $(this).find('p').attr('color','#404041');
+    $(this).find('.text').css('color','#404041');
+    $(this).find('.link').css('color','#404041');
     $(this).find('path').attr('fill','#b8b8b8');
   })
 }
