f5be0b554355927537c07d88acc14eb93a3bee99
Sunny Israni <sunny@zoraab.com>
2014-10-13 15:11:28 -0400

    setting up featured blog section

--- scripts/blog.js
+++ scripts/blog.js
@@ -8,19 +8,47 @@ $(document).ready(function(){
 var BlogController = {
   init: function(){
     this.prevewHover()
-    
+    this.popBlogs()
   }, 
   prevewHover: function(){
-    $('.blog-preview').hover(function(){
+    // $(document).on('hover','.blog-preview',function(){
+    //   $(this).find('.preview-text').addClass('preview-text-hover')
+    // }, function(){
+    //   $(this).find('.preview-text').removeClass('preview-text-hover')
+    // })
+    $(document).on('mouseover','.blog-preview',function(){
       $(this).find('.preview-text').addClass('preview-text-hover')
-    }, function(){
+    }).on('mouseout',function(){
       $(this).find('.preview-text').removeClass('preview-text-hover')
     })
+  },
+  popBlogs: function(){
+    blogs = BlogModel.getBlogs()
   }
 }
 
 var BlogModel = {
   heroUrl: function(){
     return $('.blog-hero')[0].src
+  },
+  getBlogs: function(){
+    url = "http://zoraab.herokuapp.com/blogs"
+    $.getJSON(url,function(res){
+      $.each(res,function(idx,blog){
+        BlogView.addBlog(blog)
+        // $('.blog-preview').last().addClass('blog-preview-revealed')
+      })
+    })
+  }
+}
+
+var BlogView = {
+  addBlog: function(blog){
+    el = "<a href='" + blog.link + "'></a>"
+    el = $(el).append("<img class='preview-img' src='" + blog.img_link + "'>")
+    el = el.append("<p class='preview-text'>" + blog.title + "</p>")
+    liEl = $("<li class='blog-preview'></li>")
+    liEl = $(liEl).append(el)
+    $(liEl).appendTo($('.blog-grid'))
   }
 }
