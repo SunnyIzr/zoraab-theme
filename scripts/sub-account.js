8d625c3a833f351a74a0e55eed8151efa4f460c1
Sunny Israni <sunny@zoraab.com>
2014-10-22 12:06:06 -0400

    changes to subscription accoutn

--- scripts/sub-account.js
+++ scripts/sub-account.js
@@ -25,8 +25,11 @@ var SubAccountView = {
     var monthNames = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ]
     startDate = new Date(data.cid.start_date)
     nextDate = new Date(data.cid.next_pmt_date)
+    cid = data.sub.cid
     $('#planInfo').html(data.cid.plan);
     $('#startDate').html(monthNames[startDate.getMonth()] +' '+startDate.getDate()+', '+startDate.getFullYear());
     $('#nextDate').html(monthNames[nextDate.getMonth()] +' '+nextDate.getDate()+', '+nextDate.getFullYear())
+    mailUrl = 'mailto:zoraabkit@zoraab.com?subject=Inquiry%20Regarding%20Subscription%20' + cid
+    $('.bottom-section a').attr('href',mailUrl)
   }
 }
