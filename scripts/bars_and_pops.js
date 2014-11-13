d1deddeed627013a694fe15790bde0f9fa49b04b
Sunny Israni <sunny@zoraab.com>
2014-11-13 14:24:25 -0500

    adding cookies for popup to only display once per session or 5 hour period

--- scripts/bars_and_pops.js
+++ scripts/bars_and_pops.js
@@ -1,12 +1,26 @@
 $(document).ready(function(){
-  setTimeout(function(){
-    // loadNotificationBar()
-  }, 3500)
-  setTimeout(function(){
-        revealSaleModal()
-  }, 750)
+  // loadBarOnLoad()
+  loadPopupOnLoad()
   closeModalBtn();
 })
+
+
+var loadBarOnLoad = function(){
+  setTimeout(function(){
+    if ( getCookie('zoraabpopup') != 'true') {
+      loadNotificationBar()
+    }
+  }, 3500)
+}
+
+var loadPopupOnLoad = function(){
+  setTimeout(function(){
+    if ( getCookie('zoraabpopup') != 'true' ) {
+      revealSaleModal()
+      
+    }
+  }, 750)
+}
 
 var loadNotificationBar = function(){
   // create the notification
@@ -23,10 +37,15 @@ var loadNotificationBar = function(){
   // show the notification
   notification.show();
   $('.normal-body').addClass('show')
+  
+  //create cookie
+  setCookie()
 }
 
 var revealSaleModal = function(){
   $('#hiddenModalBtn').click()
+  //create cookie
+  setCookie()
 }
 
 var closeModalBtn = function(){
@@ -34,4 +53,25 @@ var closeModalBtn = function(){
     e.preventDefault();
     $('.md-show').removeClass('md-show')
   })
+}
+
+var setCookie = function(){
+  var now = new Date();
+  var time = now.getTime();
+  // time += 18000 * 1000;
+  time += 120 * 1000
+  now.setTime(time);
+  console.log("zoraabpopup=true; " + now.toUTCString() + ";")
+  document.cookie = "zoraabpopup=true; expires=" + now.toUTCString() + "; path=/"
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0; i<ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1);
+        if (c.indexOf(name) != -1) return c.substring(name.length,c.length);
+    }
+    return "";
 }
