2998399c267f2318f09874d47060270412d9be82
Sunny Israni <sunny@zoraab.com>
2014-11-14 11:20:36 -0500

    adding basic sub form

--- scripts/subscription.js
+++ scripts/subscription.js
@@ -1,13 +1,56 @@
 SubscriptionController = {
   init: function(){
-    
+    this.activateFullForm()
+    this.singlePlanSelect()
+    this.launchSubForm()
+  },
+  activateFullForm: function(){
+    (function() {
+        var formWrap = document.getElementById( 'fs-form-wrap' );
+
+        [].slice.call( document.querySelectorAll( 'select.cs-select' ) ).forEach( function(el) {  
+          new SelectFx( el, {
+            stickyPlaceholder: false,
+            onChange: function(val){
+              document.querySelector('span.cs-placeholder').style.backgroundColor = val;
+            }
+          });
+        } );
+
+        new FForm( formWrap, {
+          onReview : function() {
+            classie.add( document.body, 'overview' ); // for demo purposes only
+          }
+        } );
+      })();
+  },
+  singlePlanSelect: function(){
+    $('.single-plan').click(function(e){
+      e.preventDefault();
+      SubscriptionView.selectPlan(this)
+      SubscriptionModel.planSelection = $(this).data('target')
+    })
+  },
+  launchSubForm: function(){
+    $('.launch-sub-form').click(function(e){
+      e.preventDefault();
+      SubscriptionView.revealForm(SubscriptionModel.planSelection)
+    })
   }
 }
 
 SubscriptionModel = {
+  planSelection: null
   
 }
 
 SubscriptionView = {
-  
+  selectPlan: function(el){
+    $('li.active').removeClass('active')
+    $(el).addClass('active')
+    $('.launch-sub-form').addClass('active')
+  },
+  revealForm: function(form){
+    $(form).addClass('active')
+  }
 }
